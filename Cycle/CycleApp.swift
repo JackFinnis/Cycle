d050a428d02319184744a6fd221cf76c870ab94f
Jack Finnis <jack.finnis@icloud.com>
2022-10-08 21:54:07 +0100

    Initial Commit

---
+++ Cycle/CycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleApp.swift
+//  Cycle
+//
+//  Created by Jack Finnis on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
